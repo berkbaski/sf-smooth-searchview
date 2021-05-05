f20371bb16b6ab41f02a55ec81f251758d7e5288
berk.baski@smartface.io <berk.baski@smartface.io>
2021-05-05 08:41:27 +0000

    updated standardList page

---
+++ scripts/components/LviElement.ts
@@ -0,0 +1,30 @@
+import LviElementDesign from 'generated/my-components/LviElement';
+
+export default class LviElement extends LviElementDesign {
+    pageName?: string | undefined;
+    constructor(props?: any, pageName?: string) {
+        // Initalizes super class for this scope
+        super(props);
+        this.pageName = pageName;
+    }
+
+    get keyText(): string {
+        return this.lblKey.text;
+    }
+
+    set keyText(key: string) {
+        this.lblKey.text = key;
+    }
+
+    get valueText(): string {
+        return this.lblValue.text;
+    }
+
+    set valueText(value: string) {
+        this.lblValue.text = value;
+    }
+
+    static getHeight(): number {
+        return 80;
+    }
+}
